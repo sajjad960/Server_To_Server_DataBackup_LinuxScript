7b6bb498f88bf27607d12d96325f5c642627973a
sajjad960 <sajjadhossainsb01@gmail.com>
2022-02-12 21:51:32 +0600

    removed uncrypted data

--- scpScript.sh
+++ scpScript.sh
@@ -4,8 +4,8 @@
 username="root"
 dir_origin="../../My-working-Directory/Test_area"
 dir_destination="Test"
-Ip="137.184.131.226"
-Port='3680'
+Ip=""
+Port=''
 
 echo "Uploading files to remote server...."
 scp -P $Port -r $dir_origin $username@$Ip:$dir_destination
